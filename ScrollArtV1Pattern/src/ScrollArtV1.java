b0dbeb80eb633190a818419f791c2648a1899532
rwanghm5 <rayden_wang@horacemann.org>
2025-10-15 09:06:33 -0400

    literally did it all

--- ScrollArtV1Pattern/src/ScrollArtV1.java
+++ ScrollArtV1Pattern/src/ScrollArtV1.java
@@ -2,27 +2,27 @@ import java.util.Random;
 
 public class ScrollArtV1 {
     static final int WIDTH = getTerminalWidth() - 1;
-    static final int BUNNY_WIDTH = 8;
-    static final int BUNNY_HEIGHT = 6;
+    static final int ROCKET_WIDTH = 12;
+    static final int ROCKET_HEIGHT = 16;
     static final Random rand = new Random();
 
     public static void main(String[] args) throws InterruptedException {
-        char[][] nextRows = new char[BUNNY_HEIGHT][WIDTH]; // store upcoming rows
+        char[][] nextRows = new char[ROCKET_HEIGHT][WIDTH]; // store upcoming rows
         for (int i = 0; i < nextRows.length; i++) {
             nextRows[i] = emptyRow();
         }
 
         while (true) {
-            for (int x = 0; x < WIDTH - BUNNY_WIDTH; x++) {
+            for (int x = 0; x < WIDTH - ROCKET_WIDTH; x++) {
                 if (rand.nextDouble() < 0.01) {
                     char[][] img;
                     if (rand.nextDouble() < 0.5)
-                        img = getBunny();
+                        img = getRocket();
                     else {
-                        img = getCarrot();
+                        img = getSword();
                     }
-                    for (int iy = 0; iy < BUNNY_HEIGHT; iy++) {
-                        for (int ix = 0; ix < BUNNY_WIDTH; ix++) {
+                    for (int iy = 0; iy < ROCKET_HEIGHT; iy++) {
+                        for (int ix = 0; ix < ROCKET_WIDTH; ix++) {
                             nextRows[iy][x + ix] = img[iy][ix];
                         }
                     }
@@ -53,75 +53,113 @@ public class ScrollArtV1 {
     }
 
     // rename your function here
-    static char[][] getBunny() {
-        char[][] img = new char[BUNNY_HEIGHT][BUNNY_WIDTH];
+    static char[][] getRocket() {
+        char[][] img = new char[ROCKET_HEIGHT][ROCKET_WIDTH];
         // fill with empty space
-        for (int y = 0; y < BUNNY_HEIGHT; y++) {
-            for (int x = 0; x < BUNNY_WIDTH; x++) {
+        for (int y = 0; y < ROCKET_HEIGHT; y++) {
+            for (int x = 0; x < ROCKET_WIDTH; x++) {
                 img[y][x] = ' ';
             }
         }
         // then fill individual characters
-        img[0][0] = '(';
-        img[0][1] = '\\';
-        img[0][2] = '(';
-        img[0][3] = '\\';
-        img[1][0] = '(';
-        img[1][1] = '-';
-        img[1][2] = '.';
-        img[1][3] = '-';
-        img[1][4] = ')';
-        img[2][0] = 'o';
-        img[2][1] = '_';
-        img[2][2] = '(';
-        img[2][3] = '"';
-        img[2][4] = ')';
-        img[2][5] = '(';
-        img[2][6] = '"';
-        img[2][7] = ')';
-        img[3][1] = '/';
-        img[3][4] = '\\';
-        img[4][1] = '\\';
-        img[4][2] = '\\';
-        img[4][3] = '/';
-        img[4][4] = '/';
-        img[5][2] = '\\';
-        img[5][3] = '/';
+        img[0][5] = '/';
+        img[0][6] = '\\';
+        img[1][4] = '/';
+        img[1][7] = '\\';
+        img[2][3] = '/';
+        img[2][8] = '\\';
+        img[3][2] = '/';
+        img[3][9] = '\\';
+        img[4][1] = '|';
+        img[4][10] = '|';
+        img[5][1] = '|';
+        img[5][10] = '|';
+        img[6][1] = '|';
+        img[6][10] = '|';
+        img[7][1] = '|';
+        img[7][10] = '|';
+        img[8][1] = '|';
+        img[8][10] = '|';
+        img[9][1] = '|';
+        img[9][10] = '|';
+        img[10][1] = '\\';
+        img[10][10] = '/';
+        img[11][2] = '\\';
+        img[11][9] = '/';
+        img[12][3] = '|';
+        img[12][4] = '|';
+        img[12][5] = '_';
+        img[12][6] = '_';
+        img[12][7] = '|';
+        img[12][8] = '|';
+        img[13][3] = '|';
+        img[13][4] = '|';
+        img[13][7] = '|';
+        img[13][8] = '|';
+        img[14][3] = '|';
+        img[14][4] = '|';
+        img[14][7] = '|';
+        img[14][8] = '|';
 
         return img;
     }
 
-    static char[][] getCarrot() {
-        char[][] img = new char[BUNNY_HEIGHT][BUNNY_WIDTH];
+    static char[][] getSword() {
+        char[][] img = new char[ROCKET_HEIGHT][ROCKET_WIDTH];
         // fill with empty space
-        for (int y = 0; y < BUNNY_HEIGHT; y++) {
-            for (int x = 0; x < BUNNY_WIDTH; x++) {
+        for (int y = 0; y < ROCKET_HEIGHT; y++) {
+            for (int x = 0; x < ROCKET_WIDTH; x++) {
                 img[y][x] = ' ';
             }
         }
         // then fill individual characters
-        img[0][5] = '\\';
-        img[0][6] = ')';
-        img[0][7] = '/';
-        img[1][4] = '-';
-        img[1][5] = '-';
-        img[1][6] = 'v';
+        img[0][2] = '/';
+        img[0][3] = '\\';
+        img[1][1] = '(';
+        img[1][4] = ')';
+        img[2][1] = '/';
+        img[2][4] = '\\';
 
-        img[2][3] = '/';
-        img[2][4] = 'r';
-        img[2][6] = ')';
-
-        img[3][2] = '>';
-        img[3][3] = 'r';
-        img[3][4] = '.';
-        img[3][5] = '/';
-
-        img[4][1] = '/';
-        img[4][3] = '\'';
-
-        img[5][0] = 'c';
-        img[5][1] = '_';
-        img[5][2] = '/';
+        img[3][0] = '|';
+        img[3][2] = '~';
+        img[3][3] = '~';
+        img[3][5] = '|';
+        img[4][0] = '|';
+        img[4][2] = '~';
+        img[4][3] = '~';
+        img[4][5] = '|';
+        img[5][0] = '|';
+        img[5][2] = '~';
+        img[5][3] = '~';
+        img[5][5] = '|';
+        img[6][0] = '|';
+        img[6][2] = '~';
+        img[6][3] = '~';
+        img[6][5] = '|';
+        img[7][0] = '|';
+        img[7][2] = '~';
+        img[7][3] = '~';
+        img[7][5] = '|';
+        img[8][0] = '|';
+        img[8][2] = '~';
+        img[8][3] = '~';
+        img[8][5] = '|';
+        img[9][0] = '(';
+        img[9][2] = '~';
+        img[9][3] = '~';
+        img[9][5] = ')';
+        img[10][0] = '\\';
+        img[10][2] = '~';
+        img[10][3] = '~';
+        img[10][5] = '/';
+        img[11][1] = '|';
+        img[11][4] = '|';
+        img[12][1] = '|';
+        img[12][4] = '|';
+        img[13][1] = '\\';
+        img[13][4] = '/';
+        img[14][2] = '\\';
+        img[14][3] = '/';
 
         return img;
     }
